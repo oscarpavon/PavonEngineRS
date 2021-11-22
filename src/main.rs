93c05f253a28e6e2c70a6997736946e6fdbd7d23
Oscar G. Pavón <pavon_oscar@outlook.com>
2021-11-22 18:42:13 -0300

    Vulkan triangle

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,16 @@
-use std::io::BufRead;
 
 
-
-/*#[path = "./engine/windows_manager/windows_manager.rs"]
+#[path = "./engine/windows_manager/windows_manager.rs"]
 mod windows_manager;
 use crate::windows_manager::manager;
 
-#[path = "./engine/renderer/rendeder.rs"]
+#[path = "./engine/renderer/renderer.rs"]
 mod renderer;
 
-use crate::renderer::pe_renderer;
-*/
 
 fn main() {
     println!("Welcome to Pavon Engine");
-    //pe_renderer::pe_renderer_init();
+    renderer::pe_renderer_init();
     //manager::pe_window_manager_loop();
     
-    let mut _line = String::new();
-    println!(":");
-    let _enter = std::io::stdin().read_line(&mut _line).unwrap();
-    println!("Your enter: {}",_line);
-    loop{
-
-    }
 }
